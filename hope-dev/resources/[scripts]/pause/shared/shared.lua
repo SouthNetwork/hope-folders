a384de8bbab1f6d09a62e08d5d9bdb9c2e0d57af
South <131378304+SouthNetwork@users.noreply.github.com>
2024-02-02 09:44:17 +0000

    Server - File's

---
+++ hope-dev/resources/[scripts]/pause/shared/shared.lua
@@ -0,0 +1,178 @@
+-----------------------------------------------------------------------------------------------------------------------------------------
+-- BOXES
+-----------------------------------------------------------------------------------------------------------------------------------------
+Boxes = {
+	{
+		["Id"] = 1,
+		["Name"] = "Caixa de Dimas",
+		["Image"] = "http://34.95.182.132/HopeImages/Pause/diamantes.png",
+		["Price"] = 100,
+		["Discount"] = 0
+	},{
+		["Id"] = 2,
+		["Name"] = "Caixa de Platinas",
+		["Image"] = "http://34.95.182.132/HopeImages/Pause/platinas.png",
+		["Price"] = 250,
+		["Discount"] = 0
+	}
+}
+-----------------------------------------------------------------------------------------------------------------------------------------
+-- CONTENTBOXES
+-----------------------------------------------------------------------------------------------------------------------------------------
+ContentBoxes = {
+	[1] = {
+		{
+			["Id"] = 1,
+			["Amount"] = 40,
+			["Image"] = "gemstone",
+			["Item"] = "gemstone",
+			["Name"] = "Diamante"
+		},{
+			["Id"] = 2,
+			["Amount"] = 60,
+			["Image"] = "gemstone",
+			["Item"] = "gemstone",
+			["Name"] = "Diamante"
+		},{
+			["Id"] = 3,
+			["Amount"] = 80,
+			["Image"] = "gemstone",
+			["Item"] = "gemstone",
+			["Name"] = "Diamante"
+		},{
+			["Id"] = 4,
+			["Amount"] = 100,
+			["Image"] = "gemstone",
+			["Item"] = "gemstone",
+			["Name"] = "Diamante"
+		},{
+			["Id"] = 5,
+			["Amount"] = 120,
+			["Image"] = "gemstone",
+			["Item"] = "gemstone",
+			["Name"] = "Diamante"
+		},{
+			["Id"] = 6,
+			["Amount"] = 140,
+			["Image"] = "gemstone",
+			["Item"] = "gemstone",
+			["Name"] = "Diamante"
+		}
+	},
+	[2] = {
+		{
+			["Id"] = 1,
+			["Amount"] = 475,
+			["Image"] = "credential",
+			["Item"] = "credential",
+			["Name"] = "Platina"
+		},{
+			["Id"] = 2,
+			["Amount"] = 525,
+			["Image"] = "credential",
+			["Item"] = "credential",
+			["Name"] = "Platina"
+		},{
+			["Id"] = 3,
+			["Amount"] = 575,
+			["Image"] = "credential",
+			["Item"] = "credential",
+			["Name"] = "Platina"
+		},{
+			["Id"] = 4,
+			["Amount"] = 625,
+			["Image"] = "credential",
+			["Item"] = "credential",
+			["Name"] = "Platina"
+		},{
+			["Id"] = 5,
+			["Amount"] = 775,
+			["Image"] = "credential",
+			["Item"] = "credential",
+			["Name"] = "Platina"
+		},{
+			["Id"] = 6,
+			["Amount"] = 925,
+			["Image"] = "credential",
+			["Item"] = "credential",
+			["Name"] = "Platina"
+		}
+	}
+}
+-----------------------------------------------------------------------------------------------------------------------------------------
+-- WORKS
+-----------------------------------------------------------------------------------------------------------------------------------------
+Works = {
+	["Delivery"] = "Coming Soon...",
+	["Transporter"] = "Coming Soon...",
+	["Trucker"] = "Coming Soon...",
+	["Fisherman"] = "Coming Soon...",
+	["Tows"] = "Coming Soon...",
+	["Driver"] = "Coming Soon...",
+	["Minerman"] = "Coming Soon...",
+	["Garbageman"] = "Coming Soon..."
+}
+-----------------------------------------------------------------------------------------------------------------------------------------
+-- PREMIUMRENEW
+-----------------------------------------------------------------------------------------------------------------------------------------
+PremiumRenew = {
+	["Price"] = 200,
+	["Value"] = 200
+}
+-----------------------------------------------------------------------------------------------------------------------------------------
+-- SHOPITENS
+-----------------------------------------------------------------------------------------------------------------------------------------
+ShopItens = {
+	["gemstone"] = {
+		["Price"] = 1,
+		["Discount"] = 0
+	},
+	["energetic"] = {
+		["Price"] = 1,
+		["Discount"] = 0
+	},
+	["premium"] = {
+		["Price"] = 200,
+		["Discount"] = 0.1
+	},
+	["premiumplate"] = {
+		["Price"] = 150,
+		["Discount"] = 5
+	},
+	["newchars"] = {
+		["Price"] = 140,
+		["Discount"] = 5
+	},
+	["namechange"] = {
+		["Price"] = 120,
+		["Discount"] = 5
+	},
+	["pendrive"] = {
+		["Price"] = 20,
+		["Discount"] = 0
+	},
+	["card01"] = {
+		["Price"] = 30,
+		["Discount"] = 0
+	},
+	["newlocate"] = {
+		["Price"] = 75,
+		["Discount"] = 4
+	},
+	["newgarage"] = {
+		["Price"] = 100,
+		["Discount"] = 0
+	},
+	["WEAPON_KATANA"] = {
+		["Price"] = 50,
+		["Discount"] = 5
+	},
+	["WEAPON_KARAMBIT"] = {
+		["Price"] = 50,
+		["Discount"] = 0
+	},
+	["chip"] = {
+		["Price"] = 120,
+		["Discount"] = 0
+	}
+}
