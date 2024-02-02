10c1e8f52c783586470a0ad085e6592618b0dece
South <131378304+SouthNetwork@users.noreply.github.com>
2024-02-02 09:43:18 +0000

    Server - File's

---
+++ hope-dev/resources/[scripts]/chat/shared-side/shared.lua
@@ -0,0 +1,36 @@
+Block = {
+	"zap",
+	"lixo",
+	"macaco",
+	"preto",
+	"arrombado",
+	"viadinho",
+	"urugutango",
+	"gorila",
+	"gorilla",
+	"mongoloide",
+	"bixa",
+	"bicha",
+	"traveco",
+	"veveco",
+	"boiola",
+	"pau",
+	"buceta",
+	"gay",
+	"piranha",
+	"monkey",
+	"vagabunda",
+	"puta",
+	"escroto",
+	"piranha",
+	"pretinho",
+	"escurinho",
+	"negrinho",
+	"piranha"
+}
+
+ClientState = {
+	["Police"] = true,
+	["Mechanic"] = true,
+	["Paramedic"] = true
+}
